814bb5a7c0b4b30471ed10474ccb903694c33f54
Albireo <kappa7194@hotmail.it>
2016-01-04 20:04:17 +0100

    Connection monitor refactoring.

--- NetworkStatusMonitor/Program.cs
+++ NetworkStatusMonitor/Program.cs
@@ -15,50 +15,41 @@
 
     public static class Program
     {
+        private readonly static IPAddress address = new IPAddress(0x08080808);
+        private readonly static byte[] buffer = new byte[0];
+        private readonly static PingOptions pingOptions = new PingOptions(32, true);
+
         public static void Main()
         {
             AppDomain.CurrentDomain.UnhandledException += HandleUnhandledException;
 
-            Log.Application.Info("Application started.");
-
-            var address = new IPAddress(0x08080808);
-            var buffer = new byte[0];
-            var pingOptions = new PingOptions(32, true);
+            Log.Application.Info("Application starting.");
+            Log.Application.Debug("Web server starting.");
 
             using (WebApp.Start("http://localhost:8080/", Startup))
             {
-                TimerCallback callback =
-                    state =>
-                    {
-                        var moment = DateTimeOffset.Now;
+                Log.Application.Debug("Web server started.");
 
-                        using (var ping = new Ping())
-                        {
-                            PingReply pingReply;
-
-                            try
-                            {
-                                pingReply = ping.Send(address, 100, buffer, pingOptions);
-                            }
-                            catch (PingException)
-                            {
-                                Log.Status.Info("{0:yyyy-MM-dd HH:mm:ss} Error 0", moment);
-
-                                return;
-                            }
-
-                            Log.Status.Info("{0:yyyy-MM-dd HH:mm:ss} {1:F} {2:D}", moment, pingReply.Status, pingReply.RoundtripTime);
-                        }
-                    };
-
-                using (var timer = new Timer(callback, null, 0, 1000))
+                using (var timer = new Timer(Callback, null, 0, 1000))
                 {
+                    Log.Application.Info("Application started.");
+
                     Console.ReadKey(true);
 
+                    Log.Application.Info("Application stopping.");
+                    Log.Application.Debug("Timer deactivating.");
+
                     timer.Change(-1, -1);
+
+                    Log.Application.Debug("Timer deactivated.");
+                    Log.Application.Debug("Timer stopping.");
                 }
+
+                Log.Application.Debug("Timer stopped.");
+                Log.Application.Debug("Web server stopping.");
             }
 
+            Log.Application.Debug("Web server stopped.");
             Log.Application.Info("Application stopped.");
         }
 
@@ -101,11 +92,11 @@
 
         private static void Startup(IAppBuilder appBuilder)
         {
-            Log.Application.Debug("Web server startup started.");
+            Log.Application.Debug("Web server configuration started.");
 
             appBuilder.Run(HandlerAsync);
 
-            Log.Application.Debug("Web server startup completed.");
+            Log.Application.Debug("Web server configuration completed.");
         }
 
         private static async Task HandlerAsync(IOwinContext context)
@@ -151,6 +142,39 @@
             }
 
             Log.Application.Debug("Resource not found handler completed.");
+        }
+
+        private static void Callback(object state)
+        {
+            Log.Application.Debug("Timer callback started.");
+
+            var moment = DateTimeOffset.Now;
+
+            using (var ping = new Ping())
+            {
+                PingReply pingReply;
+
+                try
+                {
+                    Log.Application.Info("Ping executing.");
+
+                    pingReply = ping.Send(address, 100, buffer, pingOptions);
+
+                    Log.Application.Info("Ping executed.");
+                }
+                catch (PingException)
+                {
+                    Log.Application.Error("Ping failed.");
+
+                    Log.Status.Info("{0:yyyy-MM-dd HH:mm:ss} Error 0", moment);
+
+                    return;
+                }
+
+                Log.Status.Info("{0:yyyy-MM-dd HH:mm:ss} {1:F} {2:D}", moment, pingReply.Status, pingReply.RoundtripTime);
+            }
+
+            Log.Application.Debug("Timer callback completed.");
         }
     }
 }
