b2c269b1cf71274c208209648089e9684531e21a
Albireo <kappa7194@hotmail.it>
2016-01-04 16:09:54 +0100

    Implement logging.

---
+++ NetworkStatusMonitor/Log.cs
@@ -0,0 +1,98 @@
+﻿namespace NetworkStatusMonitor
+{
+    using System.Globalization;
+
+    using NLog;
+
+    public class Log
+    {
+        private readonly ILogger logger;
+
+        private readonly static Log ApplicationInstance = new Log("application");
+        private readonly static Log ExceptionsInstance = new Log("exceptions");
+        private readonly static Log StatusInstance = new Log("status");
+
+        private Log(string loggerName)
+        {
+            this.logger = LogManager.GetLogger(loggerName);
+        }
+
+        public static Log Application => ApplicationInstance;
+
+        public static Log Exceptions => ExceptionsInstance;
+
+        public static Log Status => StatusInstance;
+
+        public void Debug(string format, params object[] args)
+        {
+            this.Write(LogLevel.Debug, format, args);
+        }
+
+        public void Debug(string message)
+        {
+            this.Write(LogLevel.Debug, message);
+        }
+
+        public void Trace(string format, params object[] args)
+        {
+            this.Write(LogLevel.Trace, format, args);
+        }
+
+        public void Trace(string message)
+        {
+            this.Write(LogLevel.Trace, message);
+        }
+
+        public void Info(string format, params object[] args)
+        {
+            this.Write(LogLevel.Info, format, args);
+        }
+
+        public void Info(string message)
+        {
+            this.Write(LogLevel.Info, message);
+        }
+
+        public void Warn(string format, params object[] args)
+        {
+            this.Write(LogLevel.Warn, format, args);
+        }
+
+        public void Warn(string message)
+        {
+            this.Write(LogLevel.Warn, message);
+        }
+
+        public void Error(string format, params object[] args)
+        {
+            this.Write(LogLevel.Error, format, args);
+        }
+
+        public void Error(string message)
+        {
+            this.Write(LogLevel.Error, message);
+        }
+
+        public void Fatal(string format, params object[] args)
+        {
+            this.Write(LogLevel.Fatal, format, args);
+        }
+
+        public void Fatal(string message)
+        {
+            this.Write(LogLevel.Fatal, message);
+        }
+
+        private void Write(LogLevel level, string format, params object[] args)
+        {
+            var message = string.Format(CultureInfo.InvariantCulture, format, args);
+
+            this.Write(level, message);
+        }
+
+        private void Write(LogLevel level, string message)
+        {
+            this.logger.Log(level, message);
+        }
+    }
+}
--- NetworkStatusMonitor/Program.cs
+++ NetworkStatusMonitor/Program.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.IO;
+    using System.Text;
     using System.Threading.Tasks;
 
     using Microsoft.Owin;
@@ -13,47 +14,107 @@
     {
         public static void Main()
         {
+            AppDomain.CurrentDomain.UnhandledException += HandleUnhandledException;
+
+            Log.Application.Info("Application started.");
+
             using (WebApp.Start("http://localhost:8080/", Startup))
             {
-                Console.ReadLine();
+                Console.ReadKey(true);
+            }
+
+            Log.Application.Info("Application stopped.");
+        }
+
+        private static void HandleUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var id = Guid.NewGuid().ToString("D");
+
+            Log.Application.Fatal("Unhandled exception ({0}).", id);
+
+            var exception = (Exception) e.ExceptionObject;
+            var message = FormatException(exception, id);
+
+            Log.Exceptions.Fatal(message);
+        }
+
+        private static string FormatException(Exception exception, string id)
+        {
+            var stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine(id);
+
+            FormatException(stringBuilder, exception);
+
+            return stringBuilder.ToString();
+        }
+
+        private static void FormatException(StringBuilder stringBuilder, Exception exception)
+        {
+            var type = exception.GetType();
+
+            stringBuilder.AppendLine(type.FullName);
+            stringBuilder.AppendLine(exception.Message);
+            stringBuilder.AppendLine(exception.StackTrace);
+
+            if (exception.InnerException != null)
+            {
+                FormatException(stringBuilder, exception.InnerException);
             }
         }
 
         private static void Startup(IAppBuilder appBuilder)
         {
+            Log.Application.Debug("Web server startup started.");
+
             appBuilder.Run(HandlerAsync);
+
+            Log.Application.Debug("Web server startup completed.");
         }
 
         private static async Task HandlerAsync(IOwinContext context)
         {
+            Log.Application.Debug("Web request handler started.");
+            Log.Application.Debug("Request path: {0}", context.Request.Path.Value);
+
             context.Response.ContentType = "text/plain";
 
             if (context.Request.Path.Value.Equals("/", StringComparison.OrdinalIgnoreCase))
             {
-                await HomePageAsync(context.Response);
+                await HomePageHandlerAsync(context.Response);
             }
             else
             {
-                await NotFoundAsync(context.Response);
+                await ResourceNotFoundHandlerAsync(context.Response);
             }
+
+            Log.Application.Debug("Web request handler completed.");
         }
 
-        private static async Task HomePageAsync(IOwinResponse response)
+        private static async Task HomePageHandlerAsync(IOwinResponse response)
         {
+            Log.Application.Debug("Home page handler started.");
+
             using (var streamWriter = new StreamWriter(response.Body))
             {
                 await streamWriter.WriteLineAsync("TODO");
             }
+
+            Log.Application.Debug("Home page handler completed.");
         }
 
-        private static async Task NotFoundAsync(IOwinResponse response)
+        private static async Task ResourceNotFoundHandlerAsync(IOwinResponse response)
         {
+            Log.Application.Debug("Resource not found handler started.");
+
             response.StatusCode = 404;
 
             using (var streamWriter = new StreamWriter(response.Body))
             {
                 await streamWriter.WriteLineAsync("There is nothing here.");
             }
+
+            Log.Application.Debug("Resource not found handler completed.");
         }
     }
 }
